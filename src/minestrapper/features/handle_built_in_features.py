5d96b54b4801057b57f77688e5c6591b36806ae9
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-06-21 11:43:35 -0400

    REFACTOR: Modularize built-in feature handling

--- src/minestrapper/features/handle_built_in_features.py
+++ src/minestrapper/features/handle_built_in_features.py
@@ -10,7 +10,19 @@ from .state_styles.state_styles import StateStyle
 if TYPE_CHECKING:
     from ..server import Server
 
+FEATURES = {
+    "state_styles": {
+        "class": StateStyle,
+        "enabled_by_default": True
+    }
+}
+
 def handle_built_in_features(server: "Server"):
-    if server.config_handler.get_config()['features']['state_styles']["feature_enabled"]["enabled"]:
-        state_style_feature = StateStyle(server)
-        state_style_feature.run()
+    for feature_name, feature_info in FEATURES.items():
+        target_feature = server.config_handler.get_config()["features"].get(feature_name)
+        if target_feature is None: continue
+
+        if target_feature.get("feature_enabled", feature_info["enabled_by_default"]):
+            feature_class = feature_info["class"]
+            feature_instance = feature_class(server)
+            feature_instance.run()
