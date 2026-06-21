47d75df10fddd08a6a294c737f9874155a2671f5
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-06-21 11:51:54 -0400

    FEAT: Add additional config keys for title_enabled and color_enabled in state_styles feature

--- src/minestrapper/features/state_styles/state_styles.py
+++ src/minestrapper/features/state_styles/state_styles.py
@@ -13,15 +13,18 @@ if TYPE_CHECKING:
 class StateStyle(Feature):
     def __init__(self, server: "Server", ):
         super().__init__("State Styles", "Provides colored text and titles for different server states", server)
-        self.config = self.server.config_handler.get_config()['features']['state_styles']["styles"]
+        self.state_styles_config = self.server.config_handler.get_config()['features']['state_styles']
+        self.styles = self.state_styles_config["styles"]
+        self.title_enabled = self.state_styles_config.get("title_enabled", False)
+        self.color_enabled = self.state_styles_config.get("color_enabled", False)
 
     def get_colored_text(self, text: str, state: ServerState) -> str:
         map = {
-            ServerState.STARTING: ANSI_COLORS.get(self.config["starting"]["color"], ANSI_COLORS["reset"]),
-            ServerState.RUNNING: ANSI_COLORS.get(self.config["running"]["color"], ANSI_COLORS["reset"]),
-            ServerState.STOPPING: ANSI_COLORS.get(self.config["stopping"]["color"], ANSI_COLORS["reset"]),
-            ServerState.STOPPED: ANSI_COLORS.get(self.config["stopped"]["color"], ANSI_COLORS["reset"]),
-            ServerState.PAUSED: ANSI_COLORS.get(self.config["paused"]["color"], ANSI_COLORS["reset"]),
+            ServerState.STARTING: ANSI_COLORS.get(self.styles["starting"]["color"], ANSI_COLORS["reset"]),
+            ServerState.RUNNING: ANSI_COLORS.get(self.styles["running"]["color"], ANSI_COLORS["reset"]),
+            ServerState.STOPPING: ANSI_COLORS.get(self.styles["stopping"]["color"], ANSI_COLORS["reset"]),
+            ServerState.STOPPED: ANSI_COLORS.get(self.styles["stopped"]["color"], ANSI_COLORS["reset"]),
+            ServerState.PAUSED: ANSI_COLORS.get(self.styles["paused"]["color"], ANSI_COLORS["reset"]),
         }
 
         color = map.get(state, ANSI_COLORS["reset"])
@@ -30,25 +33,27 @@ class StateStyle(Feature):
     
     def get_title(self, state: ServerState) -> str:
         map = {
-            ServerState.STARTING: self.config["starting"].get("title", "Starting"),
-            ServerState.RUNNING: self.config["running"].get("title", "Running"),
-            ServerState.STOPPING: self.config["stopping"].get("title", "Stopping"),
-            ServerState.STOPPED: self.config["stopped"].get("title", "Stopped"),
-            ServerState.PAUSED: self.config["paused"].get("title", "Paused"),
+            ServerState.STARTING: self.styles["starting"].get("title", "Starting"),
+            ServerState.RUNNING: self.styles["running"].get("title", "Running"),
+            ServerState.STOPPING: self.styles["stopping"].get("title", "Stopping"),
+            ServerState.STOPPED: self.styles["stopped"].get("title", "Stopped"),
+            ServerState.PAUSED: self.styles["paused"].get("title", "Paused"),
         }
 
         return map.get(state, "Unknown")
     
     def run(self):
-        @self.server.add_line_transformer
-        def state_styles_transformer(line: str):
-            return self.get_colored_text(line, self.server.state_handler.get())
+        if (self.color_enabled):
+            @self.server.add_line_transformer
+            def state_styles_transformer(line: str):
+                return self.get_colored_text(line, self.server.state_handler.get())
         
-        @self.server.state_handler.on_state_change
-        def set_terminal_title():
-            title = self.get_title(self.server.state_handler.get())
-            if (os.name == "nt"):
-                ctypes.windll.kernel32.SetConsoleTitleW(title)
-            else:
-                sys.stdout.write(f'\x1b]0;{title}\x07')
-                sys.stdout.flush()
+        if (self.title_enabled):
+            @self.server.state_handler.on_state_change
+            def set_terminal_title():
+                title = self.get_title(self.server.state_handler.get())
+                if (os.name == "nt"):
+                    ctypes.windll.kernel32.SetConsoleTitleW(title)
+                else:
+                    sys.stdout.write(f'\x1b]0;{title}\x07')
+                    sys.stdout.flush()
