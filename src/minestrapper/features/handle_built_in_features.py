a0e90e6aebc49207d58c2e8ab4ee4e48441bac15
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-06-21 00:38:24 -0400

    FEAT: Make state_styles read from the config file instead of hardcoding and implement title setting

--- src/minestrapper/features/handle_built_in_features.py
+++ src/minestrapper/features/handle_built_in_features.py
@@ -1,12 +1,27 @@
+import os
+import sys
+
+import ctypes
+
 from typing import TYPE_CHECKING
 
-from .state_styles.state_styles import get_colored_text
+from .state_styles.state_styles import StateStyle
 
 if TYPE_CHECKING:
     from ..server import Server
 
 def handle_built_in_features(server: "Server"):
     if server.config_handler.get_config()['features']['state_styles']["feature_enabled"]["enabled"]:
+        state_style = StateStyle(server.config_handler)
         @server.add_line_transformer
         def state_styles_transformer(line: str):
-            return get_colored_text(line, server.server_state)
+            return state_style.get_colored_text(line, server.server_state)
+        
+        @server.on_state_change
+        def set_terminal_title():
+            title = state_style.get_title(server.server_state)
+            if (os.name == "nt"):
+                ctypes.windll.kernel32.SetConsoleTitleW(title)
+            else:
+                sys.stdout.write(f'\x1b]0;{title}\x07')
+                sys.stdout.flush()
--- src/minestrapper/features/state_styles/state_styles.py
+++ src/minestrapper/features/state_styles/state_styles.py
@@ -1,16 +1,36 @@
-from typing import Callable
+from typing import TYPE_CHECKING, Callable
 
 from minestrapper.util.ansi_colors import ANSI_COLORS
 from minestrapper.states import ServerState
 
-def get_colored_text(text: str, state: ServerState) -> str:
-    map = {
-        ServerState.STARTING: ANSI_COLORS["yellow"],
-        ServerState.RUNNING: ANSI_COLORS["green"],
-        ServerState.STOPPING: ANSI_COLORS["red"],
-        ServerState.STOPPED: ANSI_COLORS["bright_red"],
-    }
+if TYPE_CHECKING:
+    from ....minestrapper.config_handler import ConfigHandler
 
-    color = map.get(state, ANSI_COLORS["reset"])
+class StateStyle:
+    def __init__(self, config_handler: 'ConfigHandler'):
+        self.config_handler = config_handler
+        self.config = self.config_handler.get_config()['features']['state_styles']["styles"]
+
+    def get_colored_text(self, text: str, state: ServerState) -> str:
+        map = {
+            ServerState.STARTING: ANSI_COLORS.get(self.config["starting"]["color"], ANSI_COLORS["reset"]),
+            ServerState.RUNNING: ANSI_COLORS.get(self.config["running"]["color"], ANSI_COLORS["reset"]),
+            ServerState.STOPPING: ANSI_COLORS.get(self.config["stopping"]["color"], ANSI_COLORS["reset"]),
+            ServerState.STOPPED: ANSI_COLORS.get(self.config["stopped"]["color"], ANSI_COLORS["reset"]),
+            ServerState.PAUSED: ANSI_COLORS.get(self.config["paused"]["color"], ANSI_COLORS["reset"]),
+        }
+
+        color = map.get(state, ANSI_COLORS["reset"])
+        
+        return f"{color}{text}{ANSI_COLORS['reset']}"
     
-    return f"{color}{text}{ANSI_COLORS['reset']}"
+    def get_title(self, state: ServerState) -> str:
+        map = {
+            ServerState.STARTING: self.config["starting"].get("title", "Starting"),
+            ServerState.RUNNING: self.config["running"].get("title", "Running"),
+            ServerState.STOPPING: self.config["stopping"].get("title", "Stopping"),
+            ServerState.STOPPED: self.config["stopped"].get("title", "Stopped"),
+            ServerState.PAUSED: self.config["paused"].get("title", "Paused"),
+        }
+
+        return map.get(state, "Unknown")
